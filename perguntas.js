4bf6ec923eb10eda301a5dfdf6d0964a022358cd
GabrielFoons <00001084574974sp@al.educacao.sp.gov.br>
2024-11-28 19:49:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'One Piece',
+    'Qual é o principal objetivo de Luffy em One Piece?',
+    'Luffy quer encontrar o One Piece, o lendário tesouro deixado pelo Rei dos Piratas, Gol D. Roger, e se tornar o próximo Rei dos Piratas.'
+)
+
+criaCartao(
+    'Attack on Titan',
+    'Quem é o assassino dos pais de Eren em Attack on Titan?',
+    'O Titã Sorridente, controlado por Dina Fritz, matou a mãe de Eren. Seu pai, Grisha Jaeger, foi morto pelo próprio Eren em um paradoxo temporal revelado mais tarde.'
+)
+
+criaCartao(
+    'My Hero Academy',
+    'Qual é a peculiaridade de Midoriya em My Hero Academia?',
+    ' Midoriya nasceu sem uma peculiaridade, mas herda o One For All de All Might, uma habilidade que acumula força e pode ser passada entre pessoas.'
+)
+
+criaCartao(
+    'Tokyo Ghoul',
+    'Por que Kaneki se torna um Ghoul em Tokyo Ghoul?',
+    ' Kaneki se torna um Ghoul após um acidente onde ele recebe os órgãos de Rize Kamishiro, uma Ghoul poderosa, durante uma cirurgia.'
+)
+criaCartao(
+    'Jujutsu Kaisen',
+    ' Qual o poder de Satoru Gojo?',
+    'Gojo Satoru possui o Six Eyes e a técnica Infinity, que o tornam quase invencível ao manipular espaço e energia amaldiçoada.'
+)
+criaCartao(
+    'HUNTER X HUNTER',
+    'Quem é o inimigo principal de Gon em Hunter x Hunter?',
+    ' O inimigo mais marcante para Gon é Hisoka, um caçador e lutador imprevisível que se interessa pelo potencial de Gon como adversário.'
+)
